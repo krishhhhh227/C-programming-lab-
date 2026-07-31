281e157bb62bf80be44e6b84c2b92891af2928ea
krishhhhh227 <krish.kumar.cse.2025@miet.ac.in>
2026-07-31 12:25:30 +0530

    Add files via upload

---
+++ matixdiff.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main(){
+    int a[10][10];
+    int b[10][10];
+    int m[10][10];
+    int i,j,r,c;
+    printf("enter size of matrix");
+    scanf("%d%d",&r,&c);
+    printf("Enter elements of firt matrix\n");
+    for (i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Enter elements of 2nd matrix\n");
+    for (i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            m[i][j]=a[i][j]-b[i][j];
+        }
+    }
+    printf("New matrix by additon of above two matrix\n");
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++){
+            printf("%d ",m[i][j]);
+        }
+        printf("\n");
+    }
+}
