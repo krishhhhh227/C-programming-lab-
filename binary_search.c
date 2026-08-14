fab96038113e13a1215b44d952017459654a3266
krishhhhh227 <krish.kumar.cse.2025@miet.ac.in>
2026-08-14 11:51:38 +0530

    Update binary_search.c

--- binary_search.c
+++ binary_search.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 int main(){
-    int a[10],i,n,target;
+    int a[10],i,n,target;\
+
+    
 
     printf("Enter total of elements : ");
     scanf("%d",&n);
-
     printf("Enter array's elements : \n");
     for (i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
+
+    for (i=0;i<n-1;i++) {
+        for (int j=0;j<n-i-1;j++) {
+            if (a[j]>a[j+1]) {
+                int temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+    for (i=0; i<n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    int flag=0;
 
     printf("Which element you want to find in ths array : ");
     scanf("%d",&target);
@@ -18,6 +37,7 @@ int main(){
         mid=(l+r)/2;
         if (a[mid]==target){
             printf("Element found at index : %d",mid);
+            flag=1;
             break;
         }
         else if (a[mid]>target){
@@ -26,6 +46,9 @@ int main(){
         else{
             l=mid+1;
         }
+    }
+    if (flag==0){
+        printf("Element not found in the array.");
     }
 
 }
